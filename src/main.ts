5a691170189afe2e0e98bdab51d9c24544dfd69d
inokawa <48897392+inokawa@users.noreply.github.com>
2024-12-21 15:15:21 +0900

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,40 @@
-import Quill from "quill";
-import QuillCursors from "quill-cursors";
-import * as Y from "yjs";
-import { QuillBinding } from "y-quill";
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import { QuillBinding } from 'y-quill'
+import Quill from 'quill'
+import QuillCursors from 'quill-cursors'
 
-Quill.register("modules/cursors", QuillCursors);
+Quill.register('modules/cursors', QuillCursors)
 
-const quill = new Quill(document.querySelector("#editor") as HTMLElement, {
+const quill = new Quill(document.querySelector('#editor') as HTMLElement, {
   modules: {
     cursors: true,
     toolbar: [
       // adding some basic Quill content features
       [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline"],
-      ["image", "code-block"],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
     ],
     history: {
       // Local undo shouldn't undo changes
       // from remote users
-      userOnly: true,
-    },
+      userOnly: true
+    }
   },
-  placeholder: "Start collaborating...",
-  theme: "snow", // 'bubble' is also great
-});
+  placeholder: 'Start collaborating...',
+  theme: 'snow' // 'bubble' is also great
+})
 
 // A Yjs document holds the shared data
-const ydoc = new Y.Doc();
-// Define a shared text type on the document
-const ytext = ydoc.getText("quill");
+const ydoc = new Y.Doc()
 
-// Create an editor-binding which
-// "binds" the quill editor to a Y.Text type.
-const binding = new QuillBinding(ytext, quill);
+const provider = new WebrtcProvider('quill-demo-room', ydoc)
+
+// Define a shared text type on the document
+const ytext = ydoc.getText('quill')
+
+// "Bind" the quill editor to a Yjs text type.
+const binding = new QuillBinding(ytext, quill, provider.awareness)
+
+// Remove the selection when the iframe is blurred
+window.addEventListener('blur', () => { quill.blur() })
